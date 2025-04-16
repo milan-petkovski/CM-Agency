c112613d69c598a1c6a2f85a23c14966de67b9c0
Milan Petkovski <milan.petkovski1969@gmail.com>
2025-04-16 23:41:38 +0200

    Motivacione fix 2

--- app.js
+++ app.js
@@ -113,7 +113,7 @@ async function checkAuth() {
           Math.floor((new Date() - startDate) / (1000 * 60 * 60 * 24)) %
             motivationalQuotes.length
         ],
-        "success"
+        "neutral"
       );
     }
 
